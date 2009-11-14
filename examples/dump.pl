ccd098671f255045a9e1c268d6b12a192d1cfcd8
xant <xant@dyne.org>
2009-11-14 09:38:42 +0100

    updating dump.pl example to fit last changes in the api

--- examples/dump.pl
+++ examples/dump.pl
@@ -4,7 +4,9 @@ use Data::Dumper;
 use Libnfc::Reader;
 use Libnfc::CONSTANTS ':all';
 my @keys = (
-# my card-specific keys
+    # default keys
+    pack("C6", 0x00,0x00,0x00,0x00,0x00,0x00),
+    pack("C6", 0xb5,0xff,0x67,0xcb,0xa9,0x51),
 
 #   ... add your keys here in the format [ keya, keyb ] ...
 #   for instance : 
@@ -13,31 +15,37 @@ my @keys = (
 #   one couple for each sector. The index inside this array must
 #   coincide with the sector number they open.
 #
-);
 
+);
 
 my $r = Libnfc::Reader->new(debug => 1);
 
 if ($r->init()) {
     printf ("Reader: %s\n", $r->name);
 
-    my $tag = $r->connectTag(IM_ISO14443A_106);
+    my $tag = $r->connect(IM_ISO14443A_106);
 
-    if ($tag && $r->{debug}) {
-        $tag->dumpInfo
+    if ($tag) {
+        $tag->dump_info;
     } else {
         warn "No TAG";
         exit -1;
     }
 
-    $tag->setKeys(@keys);
+    $tag->set_keys(@keys);
 
+    $tag->select;
+    open(DUMP, ">./dump") or die "Can't open dump file: $!";
     for (my $i = 0; $i < $tag->blocks; $i++) {
-        if (my $data = $tag->readBlock($i)) {
-            printf("%3d : ". "%02x " x length($data) . "\n", 
-                $i, unpack("C".length($data), $data));
+        if (my $data = $tag->read_block($i)) {
+            print DUMP $data;
         } else {
-            warn $tag->error;
+            warn $tag->error."\n";
+            if ($tag->type eq "4K") {
+                print DUMP "" x 16
+            }
         }
     }
+    close(DUMP);
 }
+
