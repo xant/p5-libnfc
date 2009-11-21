138bd950ffede7609455969f180baa213bc8718f
xant <xant@dyne.org>
2009-11-21 23:53:23 +0100

    use usleep to allow using fractions of seconds as polling frequency

--- examples/poll_tag.pl
+++ examples/poll_tag.pl
@@ -4,10 +4,12 @@ use strict;
 use Libnfc::Reader;
 use Libnfc::Constants;
 use Data::Dumper;
+use Time::HiRes qw(usleep);
 use POSIX;
 
 $::DEBUG = 0;
 $::quit = 0;
+$::polling_frequency = 0.5;
 
 my $sigset = POSIX::SigSet->new(SIGINT, SIGQUIT);
 my $sigaction = POSIX::SigAction->new( \&graceful_quit, $sigset, &POSIX::SA_NOCLDSTOP );
@@ -18,9 +20,9 @@ my $r = Libnfc::Reader->new( debug => $::DEBUG ) or die "Can't connect to reader
 while (!$::quit) {
     if (my $tag = $r->connect) {
         printf "Tag: %s\n", join ':', map { sprintf("%02x", $_) } @{$tag->uid};
-        sleep 1 while (!$::quit and $tag->ping);
+        usleep $::polling_frequency while (!$::quit and $tag->ping);
     } else {
-        sleep 1; # polling frequency
+        sleep $::polling_frequency;
     }
 }
 undef($r); # ensure calling DESTROY
