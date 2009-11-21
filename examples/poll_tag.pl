847b872b9183528ecf30f047654e83033f584bfb
xant <xant@dyne.org>
2009-11-21 23:48:16 +0100

    make this example a little more elaborate.
    - Hook SIGINT and SIGQUIT to exit cleanly avoiding to leave the
      reader in an unusable state.
    - Avoid blocking wait on tags to allow exiting if no tags are in the
      field.

--- examples/poll_tag.pl
+++ examples/poll_tag.pl
@@ -1,15 +1,31 @@
 #!/usr/bin/perl
 
+use strict;
 use Libnfc::Reader;
 use Libnfc::Constants;
 use Data::Dumper;
+use POSIX;
 
-my $DEBUG = 0;
+$::DEBUG = 0;
+$::quit = 0;
 
-my $r = Libnfc::Reader->new( debug => $DEBUG );
-while (1) {
-    if (my $tag = $r->connect(IM_ISO14443A_106, 1)) {
+my $sigset = POSIX::SigSet->new(SIGINT, SIGQUIT);
+my $sigaction = POSIX::SigAction->new( \&graceful_quit, $sigset, &POSIX::SA_NOCLDSTOP );
+POSIX::sigaction(SIGINT, $sigaction);
+POSIX::sigaction(SIGQUIT, $sigaction);
+
+my $r = Libnfc::Reader->new( debug => $::DEBUG ) or die "Can't connect to reader";
+while (!$::quit) {
+    if (my $tag = $r->connect) {
         printf "Tag: %s\n", join ':', map { sprintf("%02x", $_) } @{$tag->uid};
-        sleep 1 while ($tag->ping);
+        sleep 1 while (!$::quit and $tag->ping);
+    } else {
+        sleep 1; # polling frequency
     }
 }
+undef($r); # ensure calling DESTROY
+exit 0;
+
+sub graceful_quit {
+    $::quit = 1;
+};
