f6f40e87e9ffbf9acf2a664484583efa1140609e
xant <xant@dyne.org>
2009-12-19 20:35:17 +0100

    forgot to add newer files (once moved into RFID namespace)

---
+++ lib/RFID/Libnfc/Constants.pm
@@ -0,0 +1,59 @@
+package RFID::Libnfc::Constants;
+
+use Exporter;
+our @ISA = qw( Exporter );
+our $VERSION = '0.03';
+
+BEGIN {    # must be defined at compile time
+        %constants = (
+
+            DCO => {    # export tag
+                DCO_HANDLE_CRC              => 0x00,
+                DCO_HANDLE_PARITY           => 0x01,
+                DCO_ACTIVATE_FIELD          => 0x10,
+                DCO_ACTIVATE_CRYPTO1        => 0x11,
+                DCO_INFINITE_SELECT         => 0x20,    
+                DCO_ACCEPT_INVALID_FRAMES   => 0x30,
+                DCO_ACCEPT_MULTIPLE_FRAMES  => 0x31
+            },
+            IM => {
+                IM_ISO14443A_106  => 0x00,
+                IM_FELICA_212     => 0x01,
+                IM_FELICA_424     => 0x02,
+                IM_ISO14443B_106  => 0x03,
+                IM_JEWEL_106      => 0x04
+            },
+            MC => {
+                MC_AUTH_A         => 0x60,
+                MC_AUTH_B         => 0x61,
+                MC_READ           => 0x30,
+                MC_WRITE          => 0xA0,
+                MC_TRANSFER       => 0xB0,
+                MC_DECREMENT      => 0xC0,
+                MC_INCREMENT      => 0xC1,
+                MC_STORE          => 0xC2
+            },
+            MU => {
+                MU_REQA           => 0x26,
+                MU_WUPA           => 0x52,
+                MU_SELECT1        => 0x93,
+                MU_SELECT2        => 0x95,
+                MU_READ           => 0x30,
+                MU_WRITE          => 0xA2,
+                MU_CWRITE         => 0xA0,
+                MU_HALT           => 0x50
+            }
+        );
+}
+
+# define the exports
+our %EXPORT_TAGS
+     = ( 'all' => [map { keys %$_ } values %constants], map { $_ => [ keys %{ $constants{$_} } ] } keys %constants );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+# exporting all constants by default ... there are just a few right now, so it's not a big problem
+our @EXPORT = @EXPORT_OK;
+
+use constant + { map { %$_ } values %constants }; 
+
+1;
---
+++ lib/RFID/Libnfc/Tag.pm
@@ -0,0 +1,148 @@
+package RFID::Libnfc::Tag;
+
+use strict;
+
+use RFID::Libnfc qw(nfc_configure nfc_initiator_select_tag nfc_initiator_deselect_tag);
+use RFID::Libnfc::Constants;
+use Data::Dumper;
+
+our $VERSION = '0.03';
+
+my %types = (
+    scalar(IM_ISO14443A_106) => 'RFID::Libnfc::Tag::ISO14443A_106'
+);
+
+sub new {
+    my ($class, $reader, $type, $blocking) = @_;
+
+    die "Invalid parameters to @{[ __PACKAGE__ ]} constructor"
+      unless ($reader 
+        and ref($reader) 
+        and UNIVERSAL::isa($reader, "RFID::Libnfc::Reader"));
+
+    $type = IM_ISO14443A_106 unless($type); #defaults to IM_ISO14443A_106
+
+    my $self = {};
+    $self->{debug} = $reader->{debug};
+    # Try to find the requested tag type
+    $self->{_last_error} = "";
+    $self->{_ti} = tag_info->new();
+    $self->{_pti} = $self->{_ti}->_to_ptr;
+    $self->{reader} = $reader;
+    nfc_configure($reader->pdi, DCO_ACTIVATE_FIELD, 0);
+    # Let the reader only try once to find a tag
+    nfc_configure($reader->pdi, DCO_INFINITE_SELECT, $blocking?1:0);
+    nfc_configure($reader->pdi, DCO_HANDLE_CRC, 1);
+    nfc_configure($reader->pdi, DCO_HANDLE_PARITY, 1);
+    # Enable field so more power consuming cards can power themselves up
+    nfc_configure($reader->pdi, DCO_ACTIVATE_FIELD, 1);
+
+    if (!nfc_initiator_select_tag($reader->pdi, $type, 0, 0, $self->{_pti}))
+    {
+        $self->{_last_error} = "No tag was found";
+        return undef;
+    } else {
+        print "Card:\t ".(split('::', $types{$type}))[2]." found\n" if $self->{debug};
+    }
+
+    if ($types{$type} && eval "require $types{$type};") {
+        my $productType = $types{$type}->type($self->{_pti});
+        if ($productType && eval "require $types{$type}::$productType;") {
+            bless $self, "$types{$type}::$productType";
+        } else {
+            warn "Unsupported product type $productType";
+            return undef;
+        }
+    } else {
+        warn "Unknown tag type $type";
+        return undef;
+    }
+
+    $self->init;
+    return $self;
+}
+
+sub error {
+    my $self = shift;
+    return $self->{_last_error};
+}
+
+sub reader {
+    my $self = shift;
+    return $self->{reader};
+}
+sub AUTOLOAD {
+    our $AUTOLOAD;
+    warn "$AUTOLOAD not implemented \n";
+    return undef;
+}
+
+sub DESTROY {
+    my $self = shift;
+    nfc_initiator_deselect_tag($self->reader->pdi);
+}
+
+# number of blocks on the tag
+sub blocks {
+    return 0;
+}
+
+# number of sectors on the tag
+sub sectors {
+    return 0;
+}
+
+1;
+__END__
+=head1 NAME
+
+RFID::Libnfc::Tag - base class for specific tag implementations
+
+=head1 SYNOPSIS
+
+  use RFID::Libnfc;
+
+  $tag = $r->connectTag(IM_ISO14443A_106);
+
+=head1 DESCRIPTION
+
+  Base class for all specific tag implementations
+
+=head2 EXPORT
+
+None by default.
+
+=head2 Exportable functions
+
+=head1 METHODS
+
+=item reader
+
+returns the current reader object ( RFID::Libnfc::Reader )
+
+=item error ()
+
+returns the underlying reader descriptor (to be used with the RFID::Libnfc procedural api)
+$pdi = $r->pdi
+
+=head1 SEE ALSO
+
+RFID::Libnfc::Tag::ISO14443A_106::ULTRA RFID::Libnfc::Tag::ISO14443A_106::4K
+RFID::Libnfc::Tag::ISO14443A_106 RFID::Libnfc::Constants RFID::Libnfc 
+
+< check also documentation for libnfc c library [ http://www.libnfc.org/documentation/introduction ] >
+
+=head1 AUTHOR
+
+xant
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by xant <xant@xant.net>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
